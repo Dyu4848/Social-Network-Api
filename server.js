b862c06a721fc492d26d1fadd3ff7fba3b6be3c1
David Yu <dyu4848@gmail.com>
2023-02-08 19:39:21 -0500

    added file structure

---
+++ server.js
@@ -0,0 +1,13 @@
+// Include all dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+// Connect server to mongoose
